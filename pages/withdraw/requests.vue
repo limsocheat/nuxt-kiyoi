fbeb0e970b7e085b8e3c011feee6648ebf7b5b9b
z3r0 <z3r0@localhost.localdomain>
2019-11-18 08:56:47 +0700

    Added Requests page in Withdraw System

--- pages/withdraw/requests.vue
+++ pages/withdraw/requests.vue
@@ -1,5 +1,59 @@
 <template>
 	<v-app>
-		ddd
+		<v-card class="mx-5 my-5">
+			<div class="card-header">
+				<h3>WITHDRAW REQUESTS</h3>
+			</div>
+			<v-divider></v-divider>
+			<v-data-table :headers="headers"></v-data-table>
+		</v-card>
 	</v-app>
 </template>
+
+<script>
+	export default {
+		data() {
+			return {
+				headers: [
+					{ 
+						text: 'Withdraw Id',
+						sortable: false,
+					},
+					{ 
+						text: 'Member Name',
+						sortable: false,
+					},
+					{ 
+						text: 'Amount Of Withdraw',
+						sortable: false,
+					},
+					{ 
+						text: 'Method',
+						sortable: false,
+					},
+					{ 
+						text: 'Amount In Method',
+						sortable: false,
+					},
+					{ 
+						text: 'Status',
+						sortable: false,
+					},
+					{ 
+						text: 'Action',
+						sortable: false,
+					}
+				]
+			}
+		}
+	}
+</script>
+
+<style lang="scss">
+.card-header {
+	padding: 7px 20px 7px 20px;
+	background: #2f353b;
+	color: #fff;
+}
+</style>
+
