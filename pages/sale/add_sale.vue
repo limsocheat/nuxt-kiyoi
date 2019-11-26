b3628b029d351123d46d7545879a5dae04a1bc00
z3r0 <z3r0@localhost.localdomain>
2019-11-26 10:56:06 +0700

    Only saleman can add sales

--- pages/sale/add_sale.vue
+++ pages/sale/add_sale.vue
@@ -115,7 +115,10 @@
 					</v-col>
 				</v-row>
 			</div>
-			<v-btn class="blue mx-5 lighten-2 mb-5 grey--text text--lighten-4">
+			<v-btn 
+				class="blue mx-5 lighten-2 mb-5 grey--text text--lighten-4"
+				v-permission="'add sales'"
+			>
 				<v-icon>mdi-check</v-icon>
 				Submit
 			</v-btn>
