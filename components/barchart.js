e388a6f9afbbf17927cfae0b67409650cad2b50e
z3r0 <z3r0@localhost.localdomain>
2019-12-03 14:28:03 +0700

    Added chart in Best Seller in Report Page

---
+++ components/barchart.js
@@ -0,0 +1,18 @@
+import { Bar } from 'vue-chartjs'
+
+export default {
+  extends: Bar,
+  mounted () {
+    // Overwriting base render method with actual data.
+    this.renderChart({
+      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+      datasets: [
+        {
+          label: 'Sale Quantity',
+          backgroundColor: '#6610f2',
+          data: [40, 20, 12, 39, 10, 40, 39, 60, 40, 20, 12, 11]
+        }
+      ]
+    })
+  }
+}
---
+++ pages/report/best_seller.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-app>
+  	<div class="small mx-auto py-5">
+  		<h3>Best Seller From October 2019 - December 2019</h3>
+    	<line-chart :chart-data="datacollection"></line-chart>
+  	</div>
+  </v-app>
+</template>
+
+<script>
+  import LineChart from '~/components/barchart.js'
+
+  export default {
+    components: {
+      LineChart
+    },
+    data () {
+      return {
+        datacollection: null
+      }
+    },
+  }
+</script>
+
+<style>
+.small {
+	width: 70%;
+}
+</style>
