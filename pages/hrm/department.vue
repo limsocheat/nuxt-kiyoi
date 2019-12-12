25360bbb9ced110b98bca8fa4c43aa8cf2fb193e
z3r0 <z3r0@localhost.localdomain>
2019-12-12 15:36:59 +0700

    Query data for for Department

--- pages/hrm/department.vue
+++ pages/hrm/department.vue
@@ -26,12 +26,13 @@
 								outlined
 								dense
 								label="Type department name"
+								v-model="form.name"
 							></v-text-field>
 						</v-col>
 						<v-card-actions>
 							<v-spacer></v-spacer>
-							<v-btn color="blue darken-1" text>Close</v-btn>
-							<v-btn color="primary">Save</v-btn>
+							<v-btn color="blue darken-1" text @click="closeDialog">Close</v-btn>
+							<v-btn color="primary" @click="createItem">Save</v-btn>
 						</v-card-actions>
 					</v-card>
 				</v-dialog>
@@ -53,23 +54,26 @@
 			</div>
 		</div>
 		<v-card>
-			<v-data-table :headers="headers" :items="items" :items-per-page="itemsPerPage" :options.sync="options" :server-items-length="total">
+			<v-data-table 
+				:headers="headers" 
+				:items="items"
+			>
 				<template v-slot:item.action="{ item }">
 					<v-tooltip bottom>
 						<template v-slot:activator="{ on }">
 							<!-- Edit Item -->
-							<v-icon left fab color="primary" v-on="on">
-								mdi-pencil
-							</v-icon>
+							<v-btn icon @click="editItem(item)" color="primary" v-on="on" outlined>
+								<v-icon small>mdi-pencil</v-icon>
+							</v-btn>
 						</template>
 						<span>Edit Supplier</span>
 					</v-tooltip>
 					<v-tooltip bottom>
 						<template v-slot:activator="{ on }">
 							<!-- Delete Item -->
-							<v-icon left fab color="primary" v-on="on">
-								mdi-delete
-							</v-icon>
+							<v-btn icon @click="deleteItem(item)" v-on="on" color="red" outlined>
+								<v-icon small>mdi-delete</v-icon>
+							</v-btn>
 						</template>
 						<span>Delete Supplier</span>
 					</v-tooltip>
@@ -83,7 +87,7 @@
 <script>
 export default {
 	created() {
-		// this.fetchData()
+		this.fetchData()
 	},
 
 	data() {
@@ -100,15 +104,78 @@ export default {
 			headers: [{
 					text: 'Department',
 					sortable: false,
+					value: 'name',
 				}, {
 					text: 'Action',
 					sortable: false,
+					value: 'action',
 				}, 
 			],
 		}
 	},
 
 	methods: {
+		fetchData() {
+			this.$axios.$get(`/api/hr-department`)
+			.then(res => {
+				this.items = res;
+				thi.total = res.total;
+				console.log(res)
+			})
+			.catch(err => {
+				console.log(err.response);
+			})
+		},
+
+		editItem (item) {
+	        this.editedIndex = this.items.indexOf(item);
+	        this.form = Object.assign({}, item);
+	        this.dialog = true
+      	},
+
+      	closeDialog() {
+      		this.dialog = false;
+      		this.editedIndex = -1;
+      		this.form = {};
+      	},
+
+      	deleteItem(item) {
+			if(confirm('Are u sure to delete it?')) {
+				this.$axios.$delete(`/api/hr-department/` + item.id)
+				.then(res => {
+					this.fetchData();
+					this.$toast.info('Succeessfully Delete');
+				})
+				.catch(err => {
+					console.log(err.response);
+				})
+			}
+		},
+
+		createItem() {
+			if(this.editedIndex > -1) {
+				this.$axios.$patch(`/api/hr-department/` + this.form.id, {
+					'name': this.form.name,
+				})
+				.then(res => {
+					this.fetchData();
+					this.closeDialog();
+					this.$toast.info('Succeessfully Updated');
+				})
+			}
+			else {
+				this.$axios.$post(`/api/hr-department`, this.form)
+				.then(res => {
+					this.form = res.data;
+					this.fetchData();
+					this.$toast.info('Succeessfully Created');
+					this.closeDialog();
+				})
+				.catch(err => {
+					console.log(err.response);
+				})
+			}
+		},
 	}
 }
 
