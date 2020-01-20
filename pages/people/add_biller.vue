6f626f314b2031196829a33be30a17771aadb3b7
z3r0 <z3r0@localhost.localdomain>
2020-01-20 14:51:45 +0700

    Add Validate form When Submit in Add_Biller Page

--- pages/people/add_biller.vue
+++ pages/people/add_biller.vue
@@ -5,68 +5,70 @@
 				<v-card-title class="white--text">ADD BILLER</v-card-title>
 			</div>
 			<v-divider></v-divider>
-			<v-row class="px-5">
-				<v-col sm="6" cols="12">
-					<label class="font-weight-bold" for="name">Name*</label>
-					<ValidationProvider rules="required|alpha" v-slot="{ errors }">
-						<input type="text" class="biller-image" required v-model="form.name" />
-						<span class="red--text">{{ errors[0] }}</span>
-					</ValidationProvider>
-				</v-col>
-				<v-col sm="6" cols="12" class="d-flex flex-column">
-					<label class="font-weight-bold" for="image">Image*</label>
-					<input type="file" accept="image/*" @change="uploadImage($event)" class="biller-image" />
-				</v-col>
-				<v-col sm="6" cols="12">
-					<label class="font-weight-bold" for>Company Name*</label>
-					<ValidationProvider rules="required|alpha" v-slot="{ errors }">
-						<input type="text" class="biller-image" required v-model="form.company_name" />
-						<span class="red--text">{{ errors[0] }}</span>
-					</ValidationProvider>
-				</v-col>
-				<v-col sm="6" cols="12">
-					<label class="font-weight-bold" for>
-						VAT Number
-						<span class="font-weight-light">(Optional)</span>
-					</label>
-					<input type="text" class="biller-image" required v-model="form.vat_number" />
-				</v-col>
-				<v-col sm="6" cols="12">
-					<label class="font-weight-bold" for>Email</label>
-					<ValidationProvider rules="required" v-slot="{ errors }">
-						<input type="text" class="biller-image" required v-model="form.email" />
-						<span class="red--text">{{ errors[0] }}</span>
-					</ValidationProvider>
-				</v-col>
-				<v-col sm="6" cols="12">
-					<label class="font-weight-bold" for>Phone Number</label>
-					<ValidationProvider rules="required" v-slot="{ errors }">
-						<input type="text" class="biller-image" required v-model="form.phone" />
-						<span class="red--text">{{ errors[0] }}</span>
-					</ValidationProvider>
-				</v-col>
-				<v-col sm="6" cols="12">
-					<label class="font-weight-bold" for>Address</label>
-					<ValidationProvider rules="required|alpha" v-slot="{ errors }">
-						<input type="text" class="biller-image" required v-model="form.address" />
-						<span class="red--text">{{ errors[0] }}</span>
-					</ValidationProvider>
-				</v-col>
-				<v-col sm="6" cols="12">
-					<label class="font-weight-bold" for>
-						City
-						<span class="font-weight-light">(Optional)</span>
-					</label>
-					<input type="text" class="biller-image" required v-model="form.city" />
-				</v-col>
-				<v-col sm="6" cols="12">
-					<label class="font-weight-bold" for>
-						Country
-						<span class="font-weight-light">(Optional)</span>
-					</label>
-					<input type="text" class="biller-image" required v-model="form.country" />
-				</v-col>
-			</v-row>
+			<ValidationObserver ref="form">
+				<v-row class="px-5">
+					<v-col sm="6" cols="12">
+						<label class="font-weight-bold" for="name">Name*</label>
+						<ValidationProvider name="Name" rules="required|alpha" v-slot="{ errors }">
+							<input type="text" class="biller-image" required v-model="form.name" />
+							<span class="red--text">{{ errors[0] }}</span>
+						</ValidationProvider>
+					</v-col>
+					<v-col sm="6" cols="12" class="d-flex flex-column">
+						<label class="font-weight-bold" for="image">Image*</label>
+						<input type="file" accept="image/*" @change="uploadImage($event)" class="biller-image" />
+					</v-col>
+					<v-col sm="6" cols="12">
+						<label class="font-weight-bold" for>Company Name*</label>
+						<ValidationProvider name="Company Name" rules="required|alpha" v-slot="{ errors }">
+							<input type="text" class="biller-image" required v-model="form.company_name" />
+							<span class="red--text">{{ errors[0] }}</span>
+						</ValidationProvider>
+					</v-col>
+					<v-col sm="6" cols="12">
+						<label class="font-weight-bold" for>
+							VAT Number
+							<span class="font-weight-light">(Optional)</span>
+						</label>
+						<input type="text" class="biller-image" required v-model="form.vat_number" />
+					</v-col>
+					<v-col sm="6" cols="12">
+						<label class="font-weight-bold" for>Email</label>
+						<ValidationProvider name="Email" rules="required|email" v-slot="{ errors }">
+							<input type="text" class="biller-image" required v-model="form.email" />
+							<span class="red--text">{{ errors[0] }}</span>
+						</ValidationProvider>
+					</v-col>
+					<v-col sm="6" cols="12">
+						<label class="font-weight-bold" for>Phone Number</label>
+						<ValidationProvider name="Phone Number" rules="required" v-slot="{ errors }">
+							<input type="text" class="biller-image" required v-model="form.phone" />
+							<span class="red--text">{{ errors[0] }}</span>
+						</ValidationProvider>
+					</v-col>
+					<v-col sm="6" cols="12">
+						<label class="font-weight-bold" for>Address</label>
+						<ValidationProvider name="Address" rules="required|alpha" v-slot="{ errors }">
+							<input type="text" class="biller-image" required v-model="form.address" />
+							<span class="red--text">{{ errors[0] }}</span>
+						</ValidationProvider>
+					</v-col>
+					<v-col sm="6" cols="12">
+						<label class="font-weight-bold" for>
+							City
+							<span class="font-weight-light">(Optional)</span>
+						</label>
+						<input type="text" class="biller-image" required v-model="form.city" />
+					</v-col>
+					<v-col sm="6" cols="12">
+						<label class="font-weight-bold" for>
+							Country
+							<span class="font-weight-light">(Optional)</span>
+						</label>
+						<input type="text" class="biller-image" required v-model="form.country" />
+					</v-col>
+				</v-row>
+			</ValidationObserver>
 			<div class="pb-5 pt-3 px-5">
 				<v-btn color="primary" v-permission="'add users'" @click="createBiller">
 					<v-icon left>mdi-check</v-icon>Submit
@@ -77,8 +79,12 @@
 </template>
 
 <script>
+	import { ValidationProvider, ValidationObserver } from "vee-validate";
 	export default {
 		name: "AddBiller",
+		components: {
+			ValidationProvider, ValidationObserver
+		},
 		data() {
 			return {
 				form: {},
@@ -96,6 +102,9 @@
 					})
 					.catch(err => {
 						console.log(err.response.data);
+						this.$refs.form.validate(
+							err.response.data.errors
+						);
 					});
 			},
 
@@ -109,14 +118,6 @@
 				});
 			}
 		},
-
-		computed: {
-			computedDateFormattedMomentjs() {
-				return this.date
-					? moment(this.date).format("dddd, MMMM Do YYYY")
-					: "";
-			}
-		}
 	};
 </script>
 
