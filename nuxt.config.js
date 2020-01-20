bc4842dea06c7deb90248d82348c04b51ebc502b
z3r0 <z3r0@localhost.localdomain>
2020-01-20 15:57:53 +0700

    Added Vee-Validate@3.x.x

--- nuxt.config.js
+++ nuxt.config.js
@@ -129,6 +129,7 @@ export default {
     /*
     ** You can extend webpack config here
     */
+    transpile: ["vee-validate/dist/rules"],
     extend (config, ctx) {
     },
   },
--- plugins/vee-validate.js
+++ plugins/vee-validate.js
@@ -1,32 +1,33 @@
-// import { ValidationProvider, extend } from 'vee-validate';
-// import { required, alpha, email, min} from 'vee-validate/dist/rules';
-
-// import Vue from 'vue';
-// Vue.component('ValidationProvider', ValidationProvider);
-
-
-// extend('required', {
-//   ...required,
-//   message: '*This field is required'
-// });
-
-// extend('alpha', {
-//   ...alpha,
-//   message: "*This field must only contain alphabetic characters"
-// });
-
-// extend('email', {
-//   ...email,
-//   message: "*This field must be a valid email"
-// });
-
-
-// extend('min', {
-// 	...min,
-// 	message: "*Name Must be 3 characters at least"
-// });
+import { ValidationProvider, ValidationObserver, extend } from 'vee-validate';
+import { required, alpha, email, min} from 'vee-validate/dist/rules';
 
 import Vue from 'vue';
-import VeeValidateLaravel from 'vee-validate-laravel';
+Vue.component('ValidationProvider', ValidationProvider);
+Vue.component('ValidationObserver', ValidationObserver);
 
-Vue.use(VeeValidateLaravel);
+
+extend('required', {
+  ...required,
+  message: '*This field is required'
+});
+
+extend('alpha', {
+  ...alpha,
+  message: "*This field must only contain alphabetic characters"
+});
+
+extend('email', {
+  ...email,
+  message: "*This field must be a valid email"
+});
+
+
+extend('min', {
+	...min,
+	message: "*Name Must be 3 characters at least"
+});
+
+// import Vue from 'vue';
+// import VeeValidateLaravel from 'vee-validate-laravel';
+
+// Vue.use(VeeValidateLaravel);
