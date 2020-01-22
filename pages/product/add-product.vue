71a865decd0c8c9b21589669d101b095827b0af8
z3r0 <z3r0@localhost.localdomain>
2020-01-22 10:15:07 +0700

    Recorrect some part in Add_Product

--- pages/product/add-product.vue
+++ pages/product/add-product.vue
@@ -1,104 +1,81 @@
 <template>
 	<v-app>
 		<v-card class="mx-5 my-5">
-			<v-card-title class="headline teal darken-2 white--text">
-				ADD PRODUCT
-			</v-card-title>
+			<v-card-title class="headline teal darken-2 white--text">ADD PRODUCT</v-card-title>
 			<v-divider></v-divider>
 			<p class="px-5 pt-3">The field labels marked with * are required input fields.</p>
-			<v-row class="px-5">
-				<v-col sm="6" cols="12">
-					<label class="font-weight-bold" for="name">Product Name*</label>
-					<v-text-field 
-						outlined solo dense
-						label="Product Name"
-						v-model="form.name"
-					></v-text-field>
-				</v-col>
-				<v-col sm="6" cols="12">
-					<label class="font-weight-bold" for="">Product Code*</label>
-					<div class="d-flex">
-						<v-text-field 
-							outlined solo dense
-							label="Product Code"
-							v-model="form.code"
-						></v-text-field>
-						<v-btn 
-							class="mx-1 mt-1" color="primary" 
-							dark 
-							@click="randomNumber"
-						>
-							Generate
-						</v-btn>
-					</div>
-				</v-col>
-				<v-col sm="6" cols="12">
-					<label class="font-weight-bold" for="">Product Type*</label>
-					<v-select 
-						outlined solo dense 
-						:items="product_type" 
-						label="Please Select"
-						v-model="form.type"
-					>
-					</v-select>
-				</v-col>
-				<v-col sm="6" cols="12">
-					<label class="font-weight-bold" for="">Brand*</label>
-					<v-autocomplete 
-						outlined 
-						solo dense 
-						:items="items"
-						item-text="brand.name"
-						item-value="brand.name"
-						label="Select Brand"
-						v-model="form.brand"
-					>
-					</v-autocomplete>
-				</v-col>
-				<v-col sm="12" cols="12">
-					<label class="font-weight-bold" for="">Barcode Symbology*</label>
-					<v-select 
-						outlined solo dense :items="barcodes" 
-						label="Please Select"
-						v-model="form.barcode"
-					></v-select>
-				</v-col>
-			</v-row>
-			<v-row class="px-5">
-				<v-col sm="6" cols="12">
-					<label class="font-weight-bold">Product Unit*</label>
-					<v-text-field 
-						outlined solo dense
-						label="Product Unit"
-						v-model="form.quantity"
-						type="number"
-					></v-text-field>
-				</v-col>
-				<v-col sm="6" cols="12">
-					<label class="font-weight-bold">Product Price*</label>
-					<v-text-field 
-						outlined solo dense
-						label="Purchase Price"
-						v-model="form.unit_price"
-						type="number"
-					></v-text-field>
-				</v-col>
-				<v-col sm="12" cols="12">
-					<label class="font-weight-bold"  for="image">Product Image</label>
-					<div v-if="url" class="preview--image">
-						<img :src="form.image" class="img-responsive" height="300">
-					</div>
-					<input type="file" @change="uploadImage($event)" class="product--image">
-				</v-col>
-				<v-col md="12" sm="12" cols="12">
-					<label class="font-weight-bold" for="">Product Details</label>
-					<vue-editor v-model="form.description"></vue-editor>
-				</v-col>
-			</v-row>
+			<ValidationObserver ref="form">
+				<v-row class="px-5">
+					<v-col sm="6" cols="12">
+						<label class="font-weight-bold" for="name">Product Name*</label>
+						<validation-provider name="Name" rules="required" v-slot="{ errors }">
+							<v-text-field outlined solo dense label="Product Name" v-model="form.name"></v-text-field>
+							<span class="red--text">{{ errors[0] }}</span>
+						</validation-provider>
+					</v-col>
+					<v-col sm="6" cols="12">
+						<label class="font-weight-bold" for>Product Code*</label>
+						<validation-provider name="Product Code" rules="required" v-slot="{ errors }">
+							<div class="d-flex">
+								<v-text-field outlined solo dense label="Product Code" v-model="form.code"></v-text-field>
+								<v-btn class="mx-1 mt-1" color="primary" dark @click="randomNumber">Generate</v-btn>
+							</div>
+							<span class="red--text">{{errors[0]}}</span>
+						</validation-provider>
+					</v-col>
+					<v-col sm="6" cols="12">
+						<label class="font-weight-bold" for>Brand*</label>
+						<v-autocomplete
+							outlined
+							solo
+							dense
+							:items="brands"
+							item-text="name"
+							item-value="name"
+							label="Select Brand"
+							v-model="form.brand"
+							return-object
+						></v-autocomplete>
+					</v-col>
+					<v-col sm="6" cols="12">
+						<label class="font-weight-bold" for>Barcode Type*</label>
+						<validation-provider rules="required" name="Barcode Type" v-slot="{ errors }">
+							<v-select outlined solo dense :items="barcodes" label="Please Select" v-model="form.barcode"></v-select>
+							<span class="red--text">{{ errors[0] }}</span>
+						</validation-provider>
+					</v-col>
+				</v-row>
+				<v-row class="px-5">
+					<v-col sm="6" cols="12">
+						<label class="font-weight-bold">Product Unit*</label>
+						<validation-provider rules="required" name="Product Unit" v-slot="{ errors }">
+							<v-text-field outlined solo dense label="Product Unit" v-model="form.unit" type="number"></v-text-field>
+							<span class="red--text">{{ errors[0] }}</span>
+						</validation-provider>
+					</v-col>
+					<v-col sm="6" cols="12">
+						<label class="font-weight-bold">Product Price*</label>
+						<validation-provider rules="required" name="Product Price" v-slot="{ errors }">
+							<v-text-field outlined solo dense label="Purchase Price" v-model="form.price" type="number"></v-text-field>
+							<span class="red--text">{{ errors[0] }}</span>
+						</validation-provider>
+					</v-col>
+					<v-col sm="12" cols="12">
+						<label class="font-weight-bold" for="image">Product Image</label>
+						<div v-if="url" class="preview--image">
+							<img :src="form.image" class="img-responsive" height="300" />
+						</div>
+						<input type="file" @change="uploadImage($event)" class="product--image" />
+					</v-col>
+					<v-col md="12" sm="12" cols="12">
+						<label class="font-weight-bold" for>Product Details</label>
+						<vue-editor v-model="form.description"></vue-editor>
+					</v-col>
+				</v-row>
+			</ValidationObserver>
 			<v-card-actions class="px-5">
 				<v-btn color="primary" @click.prevent="createItem">
-					<v-icon>mdi-check</v-icon>
-					Create
+					<v-icon>mdi-check</v-icon>Create
 				</v-btn>
 			</v-card-actions>
 		</v-card>
@@ -111,6 +88,7 @@
 		name: "AddProduct",
 		created() {
 			this.fetchData()
+			this.fetchBrand()
 		},
 
 		data() {
@@ -119,27 +97,30 @@
 					code: '',
 				},
 				items: [],
+				brands: [],
 				url: null,
 				itemsPerPage: 5,
-				product_type: 
-				[
-					'Standard', 'Combo', 'Digital',			
-				],
 				barcodes: ['Code 128', 'Code 39', 'UPC-A', 'UPC-E', 'EAN-8', 'EAN-13'],
-				tax: ['No Tax', 'vat@10', 'vat@15', 'vat@20'],
-				tax_method: ['Exclusive', 'Inclusive'],
 			}
 		},
-		computed: {
-		   	computedDateFormattedMomentjs () {
-		        return this.date ? moment(this.date).format('dddd, MMMM Do YYYY') : ''
-		    },
-    	},
+		
 
     	methods: {
 		    randomNumber() {
 		     	return this.form.code = Math.floor(1000000 + Math.random() * 90000000)
 		    },
+
+			fetchBrand() {
+				this.$axios.$get(`api/brand`)
+				.then(res => {
+					this.$set(this.$data, 'brands', res.brands.data);
+					console.log(res);
+				})
+				.catch(err => {
+					console.log(err.response);
+					
+				});
+			},
 
 		    fetchData() {
 		    	this.$axios.$get(`api/product`)
@@ -154,10 +135,13 @@
 		    	.then(res => {
 		    		this.items = res.data;
 		    		this.$toast.info('Succeessfully created');
-		    		this.$router.push('/product/product-list');
+		    		// this.$router.push('/product/product-list');
 		    	})
 		    	.catch(err => {
-		    		console.log(err.response);
+					console.log(err.response);
+					this.$refs.form.validate(
+						err.response.data.errors
+					);
 		    	})
 		    },
 
@@ -179,7 +163,7 @@
 
 <style lang="scss">
 	.product--image {
-		border: 1px solid rgba(0,0,0,0.125);
+		border: 1px solid rgba(0, 0, 0, 0.125);
 		padding: 5px 5px 5px 5px;
 		width: 100%;
 	}
