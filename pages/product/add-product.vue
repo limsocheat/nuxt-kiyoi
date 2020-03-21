3cd9425a00efff6090dd4a3e1eeb60a3635726c4
N0SYS73M1SS4F3 <cambokid75@gmail.com>
2020-03-21 14:14:28 +0700

    Uplode Image Using FormData

--- pages/product/add-product.vue
+++ pages/product/add-product.vue
@@ -6,25 +6,65 @@
 			<p class="px-5 pt-3">The field labels marked with * are required input fields.</p>
 			<ValidationObserver ref="form">
 				<v-row class="px-5">
-					<v-col sm="6" cols="12">
-						<label class="font-weight-bold" for="name">Product Name*</label>
-						<validation-provider name="Name" rules="required" v-slot="{ errors }">
-							<v-text-field outlined solo dense label="Product Name" v-model="form.name"></v-text-field>
+					<v-col
+						sm="6"
+						cols="12"
+					>
+						<label
+							class="font-weight-bold"
+							for="name"
+						>Product Name*</label>
+						<validation-provider
+							name="Name"
+							rules="required"
+							v-slot="{ errors }"
+						>
+							<v-text-field
+								outlined
+								solo
+								dense
+								label="Product Name"
+								v-model="form.name"
+							></v-text-field>
 							<span class="red--text">{{ errors[0] }}</span>
 						</validation-provider>
 					</v-col>
-					<v-col sm="6" cols="12">
-						<label class="font-weight-bold" for>Product Code*</label>
-						<validation-provider name="Product Code" rules="required" v-slot="{ errors }">
+					<v-col
+						sm="6"
+						cols="12"
+					>
+						<label
+							class="font-weight-bold"
+							for
+						>Product Code*</label>
+						<validation-provider
+							name="Product Code"
+							rules="required"
+							v-slot="{ errors }"
+						>
 							<div class="d-flex">
-								<v-text-field outlined solo dense label="Product Code" v-model="form.code"></v-text-field>
-								<v-btn class="mx-1 mt-1" color="primary" dark @click="randomNumber">Generate</v-btn>
+								<v-text-field
+									outlined
+									solo
+									dense
+									label="Product Code"
+									v-model="form.code"
+								></v-text-field>
+								<v-btn
+									class="mx-1 mt-1"
+									color="primary"
+									dark
+									@click="randomNumber"
+								>Generate</v-btn>
 							</div>
 							<span class="red--text">{{errors[0]}}</span>
 						</validation-provider>
 					</v-col>
-					<v-col sm="6" cols="12">
-						<label class="font-weight-bold" for>Category</label>
+					<v-col
+						sm="6"
+						cols="12"
+					>
+						<label class="font-weight-bold">Category</label>
 						<v-autocomplete
 							outlined
 							solo
@@ -37,44 +77,116 @@
 							return-object
 						></v-autocomplete>
 					</v-col>
-					<v-col sm="6" cols="12">
-						<label class="font-weight-bold" for>Barcode Type*</label>
-						<validation-provider rules="required" name="Barcode Type" v-slot="{ errors }">
-							<v-select outlined solo dense :items="barcodes" label="Please Select" v-model="form.barcode"></v-select>
+					<v-col
+						sm="6"
+						cols="12"
+					>
+						<label
+							class="font-weight-bold"
+							for
+						>Barcode Type*</label>
+						<validation-provider
+							rules="required"
+							name="Barcode Type"
+							v-slot="{ errors }"
+						>
+							<v-select
+								outlined
+								solo
+								dense
+								:items="barcodes"
+								label="Please Select"
+								v-model="form.barcode"
+							></v-select>
 							<span class="red--text">{{ errors[0] }}</span>
 						</validation-provider>
 					</v-col>
 				</v-row>
 				<v-row class="px-5">
-					<v-col sm="6" cols="12">
+					<v-col
+						sm="6"
+						cols="12"
+					>
 						<label class="font-weight-bold">Product Unit*</label>
-						<validation-provider rules="required" name="Product Unit" v-slot="{ errors }">
-							<v-text-field outlined solo dense label="Product Unit" v-model="form.unit" type="number"></v-text-field>
+						<validation-provider
+							rules="required"
+							name="Product Unit"
+							v-slot="{ errors }"
+						>
+							<v-text-field
+								outlined
+								solo
+								dense
+								label="Product Unit"
+								v-model="form.unit"
+								type="number"
+							></v-text-field>
 							<span class="red--text">{{ errors[0] }}</span>
 						</validation-provider>
 					</v-col>
-					<v-col sm="6" cols="12">
+					<v-col
+						sm="6"
+						cols="12"
+					>
 						<label class="font-weight-bold">Product Price*</label>
-						<validation-provider rules="required" name="Product Price" v-slot="{ errors }">
-							<v-text-field outlined solo dense label="Purchase Price" v-model="form.price" type="number"></v-text-field>
+						<validation-provider
+							rules="required"
+							name="Product Price"
+							v-slot="{ errors }"
+						>
+							<v-text-field
+								outlined
+								solo
+								dense
+								label="Purchase Price"
+								v-model="form.price"
+								type="number"
+							></v-text-field>
 							<span class="red--text">{{ errors[0] }}</span>
 						</validation-provider>
 					</v-col>
-					<v-col sm="12" cols="12">
-						<label class="font-weight-bold" for="image">Product Image</label>
-						<div v-if="url" class="preview--image">
-							<img :src="form.image" class="img-responsive" height="300" />
+					<v-col
+						sm="12"
+						cols="12"
+					>
+						<label
+							class="font-weight-bold"
+							for="image"
+						>Product Image</label>
+						<div
+							v-if="url"
+							class="preview--image"
+						>
+							<img
+								:src="form.image"
+								class="img-responsive"
+								height="300"
+							/>
 						</div>
-						<input type="file" @change="uploadImage($event)" class="product--image" />
+						<input
+							type="file"
+							@change="uploadImage($event)"
+							class="product--image"
+						/>
 					</v-col>
-					<v-col md="12" sm="12" cols="12">
-						<label class="font-weight-bold" for>Product Details</label>
+					<v-col
+						md="12"
+						sm="12"
+						cols="12"
+					>
+						<label
+							class="font-weight-bold"
+							for
+						>Product Details</label>
 						<vue-editor v-model="form.description"></vue-editor>
 					</v-col>
 				</v-row>
 			</ValidationObserver>
 			<v-card-actions class="px-5">
-				<v-btn color="primary" @click.prevent="createItem">
+				<v-btn
+					color="primary"
+					@click.prevent="createItem"
+				>
 					<v-icon>mdi-check</v-icon>Create
 				</v-btn>
 			</v-card-actions>
@@ -83,95 +195,104 @@
 </template>
 
 <script>
-	import moment from 'moment';
+	import moment from "moment";
 	export default {
 		name: "AddProduct",
 		created() {
-			this.fetchData()
-			// this.fetchBrand()
-			this.getCategory()
+			this.fetchData();
+			this.getCategory();
 		},
 
 		data() {
 			return {
 				form: {
-					code: '',
+					code: ""
 				},
 				items: [],
-				brands: [],
 				url: null,
 				categories: [],
 				itemsPerPage: 5,
-				barcodes: ['Code 128', 'Code 39', 'UPC-A', 'UPC-E', 'EAN-8', 'EAN-13'],
-			}
+				barcodes: [
+					"Code 128",
+					"Code 39",
+					"UPC-A",
+					"UPC-E",
+					"EAN-8",
+					"EAN-13"
+				],
+				fiie: ""
+			};
 		},
-		
 
-    	methods: {
-		    randomNumber() {
-		     	return this.form.code = Math.floor(1000000 + Math.random() * 90000000)
-		    },
+		methods: {
+			randomNumber() {
+				return (this.form.code = Math.floor(
+					1000000 + Math.random() * 90000000
+				));
+			},
 
-		    getCategory() {
-		    	this.$axios.$get(`api/category`)
-		    	.then(res => {
-		    		this.$set(this.$data, 'categories', res.categories.data)
-		    		console.log(res);
-		    	})
-		    	.catch(err => {
-		    		console.log(err.response);
-		    	})
-		    },
+			getCategory() {
+				this.$axios
+					.$get(`api/category`)
+					.then(res => {
+						this.$set(this.$data, "categories", res.categories.data);
+						console.log(res);
+					})
+					.catch(err => {
+						console.log(err.response);
+					});
+			},
 
-			fetchBrand() {
-				this.$axios.$get(`api/brand`)
-				.then(res => {
-					this.$set(this.$data, 'brands', res.brands.data);
+			fetchData() {
+				this.$axios.$get(`api/product`).then(res => {
+					this.items = res.data;
 					console.log(res);
-				})
-				.catch(err => {
-					console.log(err.response);
-					
 				});
 			},
 
-		    fetchData() {
-		    	this.$axios.$get(`api/product`)
-		    	.then(res => {
-		    		this.items = res.data;
-		    		console.log(res);
-		    	})
-		    },
+			createItem() {
+				let rawData = {
+					code: this.form.code,
+					name: this.form.name,
+					barcode: this.form.barcode,
+					unit: this.form.unit,
+					price: this.form.price,
+					description: this.form.description
+				};
+				let fd = new FormData();
 
-		    createItem() {
-		    	this.$axios.$post(`api/product`, this.form)
-		    	.then(res => {
-		    		this.items = res.data;
-		    		this.$toast.info('Succeessfully created');
-		    		this.$router.push('/product/product-list');
-		    	})
-		    	.catch(err => {
-					console.log(err.response);
-					this.$refs.form.validate(
-						err.response.data.errors
-					);
-		    	})
-		    },
+				Object.keys(rawData).map(e => {
+					fd.append(e, rawData[e]);
+				});
 
-		    uploadImage(e) {
-                const images = e.target.files[0];
-                const reader = new FileReader();
+				fd.append("image", this.file);
 
-                reader.readAsDataURL(images);
-                reader.onload = e =>  {
-                    this.form.image = e.target.result;
-                    console.log(this.form);
-                }
+				// append Request Object
+				fd.append("category", JSON.stringify(this.form.category));
 
-                this.url = URL.createObjectURL(images)
-            },
-    	}
-	}
+				this.$axios
+					.$post(`api/product`, fd, {
+						headers: {
+							"Content-Type": "multipart/form-data"
+						}
+					})
+					.then(res => {
+						this.items = res.data;
+						this.$toast.info("Succeessfully created");
+						this.$router.push("/product/product-list");
+					})
+					.catch(err => {
+						console.log(err.response);
+						this.$refs.form.validate(err.response.data.errors);
+					});
+			},
+
+			uploadImage(e) {
+				this.file = e.target.files[0];
+				console.log(this.file);
+			}
+		}
+	};
 </script>
 
 <style lang="scss">
--- pages/product/product-list/_id/edit.vue
+++ pages/product/product-list/_id/edit.vue
@@ -6,75 +6,184 @@
 			<p class="px-5 pt-3">The field labels marked with * are required input fields.</p>
 			<ValidationObserver ref="form">
 				<v-row class="px-5">
-					<v-col sm="6" cols="12">
-						<label class="font-weight-bold" for="name">Product Name*</label>
-						<validation-provider name="Name" rules="required" v-slot="{ errors }">
-							<v-text-field outlined solo dense label="Product Name" v-model="form.name"></v-text-field>
+					<v-col
+						sm="6"
+						cols="12"
+					>
+						<label
+							class="font-weight-bold"
+							for="name"
+						>Product Name*</label>
+						<validation-provider
+							name="Name"
+							rules="required"
+							v-slot="{ errors }"
+						>
+							<v-text-field
+								outlined
+								solo
+								dense
+								label="Product Name"
+								v-model="form.name"
+							></v-text-field>
 							<span class="red--text">{{ errors[0] }}</span>
 						</validation-provider>
 					</v-col>
-					<v-col sm="6" cols="12">
-						<label class="font-weight-bold" for>Product Code*</label>
-						<validation-provider name="Product Code" rules="required" v-slot="{ errors }">
+					<v-col
+						sm="6"
+						cols="12"
+					>
+						<label
+							class="font-weight-bold"
+							for
+						>Product Code*</label>
+						<validation-provider
+							name="Product Code"
+							rules="required"
+							v-slot="{ errors }"
+						>
 							<div class="d-flex">
-								<v-text-field outlined solo dense label="Product Code" v-model="form.code"></v-text-field>
-								<v-btn class="mx-1 mt-1" color="primary" dark @click="randomNumber">Generate</v-btn>
+								<v-text-field
+									outlined
+									solo
+									dense
+									label="Product Code"
+									v-model="form.code"
+								></v-text-field>
+								<v-btn
+									class="mx-1 mt-1"
+									color="primary"
+									dark
+									@click="randomNumber"
+								>Generate</v-btn>
 							</div>
 							<span class="red--text">{{errors[0]}}</span>
 						</validation-provider>
 					</v-col>
-					<v-col sm="6" cols="12">
-						<label class="font-weight-bold" for>Brand*</label>
+					<v-col
+						sm="6"
+						cols="12"
+					>
+						<label class="font-weight-bold">Category</label>
 						<v-autocomplete
 							outlined
 							solo
 							dense
-							:items="brands"
+							:items="categories"
 							item-text="name"
-							item-value="name"
-							label="Select Brand"
-							v-model="form.brand"
+							item-value="id"
+							label="Select Category"
+							v-model="form.category"
 							return-object
 						></v-autocomplete>
 					</v-col>
-					<v-col sm="6" cols="12">
-						<label class="font-weight-bold" for>Barcode Type*</label>
-						<validation-provider rules="required" name="Barcode Type" v-slot="{ errors }">
-							<v-select outlined solo dense :items="barcodes" label="Please Select" v-model="form.barcode"></v-select>
+					<v-col
+						sm="6"
+						cols="12"
+					>
+						<label
+							class="font-weight-bold"
+							for
+						>Barcode Type*</label>
+						<validation-provider
+							rules="required"
+							name="Barcode Type"
+							v-slot="{ errors }"
+						>
+							<v-select
+								outlined
+								solo
+								dense
+								:items="barcodes"
+								label="Please Select"
+								v-model="form.barcode"
+							></v-select>
 							<span class="red--text">{{ errors[0] }}</span>
 						</validation-provider>
 					</v-col>
 				</v-row>
 				<v-row class="px-5">
-					<v-col sm="6" cols="12">
+					<v-col
+						sm="6"
+						cols="12"
+					>
 						<label class="font-weight-bold">Product Unit*</label>
-						<validation-provider rules="required" name="Product Unit" v-slot="{ errors }">
-							<v-text-field outlined solo dense label="Product Unit" v-model="form.unit" type="number"></v-text-field>
+						<validation-provider
+							rules="required"
+							name="Product Unit"
+							v-slot="{ errors }"
+						>
+							<v-text-field
+								outlined
+								solo
+								dense
+								label="Product Unit"
+								v-model="form.unit"
+								type="number"
+							></v-text-field>
 							<span class="red--text">{{ errors[0] }}</span>
 						</validation-provider>
 					</v-col>
-					<v-col sm="6" cols="12">
+					<v-col
+						sm="6"
+						cols="12"
+					>
 						<label class="font-weight-bold">Product Price*</label>
-						<validation-provider rules="required" name="Product Price" v-slot="{ errors }">
-							<v-text-field outlined solo dense label="Purchase Price" v-model="form.price" type="number"></v-text-field>
+						<validation-provider
+							rules="required"
+							name="Product Price"
+							v-slot="{ errors }"
+						>
+							<v-text-field
+								outlined
+								solo
+								dense
+								label="Purchase Price"
+								v-model="form.price"
+								type="number"
+							></v-text-field>
 							<span class="red--text">{{ errors[0] }}</span>
 						</validation-provider>
 					</v-col>
-					<v-col sm="12" cols="12">
-						<label class="font-weight-bold" for="image">Product Image</label>
+					<v-col
+						sm="12"
+						cols="12"
+					>
+						<label
+							class="font-weight-bold"
+							for="image"
+						>Product Image</label>
 						<div v-if="url">
-							<img :src="form.image" class="img-responsive" height="300" />
+							<img
+								:src="form.image"
+								class="img-responsive"
+								height="300"
+							/>
 						</div>
-						<input type="file" @change="uploadImage($event)" class="product--image" />
+						<input
+							type="file"
+							@change="uploadImage($event)"
+							class="product--image"
+						/>
 					</v-col>
-					<v-col md="12" sm="12" cols="12">
-						<label class="font-weight-bold" for>Product Details</label>
+					<v-col
+						md="12"
+						sm="12"
+						cols="12"
+					>
+						<label
+							class="font-weight-bold"
+							for
+						>Product Details</label>
 						<vue-editor v-model="form.description" />
 					</v-col>
 				</v-row>
 			</ValidationObserver>
 			<v-card-actions class="px-5">
-				<v-btn color="primary" @click.prevent="updateItem">
+				<v-btn
+					color="primary"
+					@click.prevent="updateItem"
+				>
 					<v-icon>mdi-check</v-icon>Create
 				</v-btn>
 			</v-card-actions>
@@ -83,95 +192,108 @@
 </template>
 
 <script>
-	import moment from 'moment';
+	import moment from "moment";
 	export default {
 		name: "EditProduct",
 		created() {
-			this.fetchData()
-			this.fetchBrand()
+			this.fetchData();
+			this.getCategory();
 		},
 
 		data() {
 			return {
-				form: {
-					description: null,
-				},
+				form: {},
 				items: [],
-				brands: [],
 				url: null,
+				file: "",
 				itemsPerPage: 5,
-				barcodes: ['Code 128', 'Code 39', 'UPC-A', 'UPC-E', 'EAN-8', 'EAN-13'],
-			}
+				categories: [],
+				barcodes: [
+					"Code 128",
+					"Code 39",
+					"UPC-A",
+					"UPC-E",
+					"EAN-8",
+					"EAN-13"
+				]
+			};
 		},
-		
 
-    	methods: {
-		    randomNumber() {
-		     	return this.form.code = Math.floor(1000000 + Math.random() * 90000000)
-		    },
-
-			fetchBrand() {
-				this.$axios.$get(`api/brand`)
-				.then(res => {
-					this.$set(this.$data, 'brands', res.brands.data);
-					console.log(res);
-				})
-				.catch(err => {
-					console.log(err.response);
-					
-				});
+		methods: {
+			randomNumber() {
+				return (this.form.code = Math.floor(
+					1000000 + Math.random() * 90000000
+				));
 			},
 
-		    fetchData() {
-		    	this.$axios.$get(`api/product/` + this.$route.params.id)
-		    	.then(res => {
-                    // this.form = res.products.data;
-                    this.$set(this.$data, 'form', res.product);
-		    		console.log(res);
-                })
-                .catch(err => {
-                    console.log(err.response);
-                })
-		    },
+			getCategory() {
+				this.$axios
+					.$get(`api/category`)
+					.then(res => {
+						this.$set(this.$data, "categories", res.categories.data);
+						console.log(res);
+					})
+					.catch(err => {
+						console.log(err.response);
+					});
+			},
 
-		    updateItem() {
-		    	this.$axios.$patch(`api/product/` + this.form.id, {
-					name: this.form.name,
+			fetchData() {
+				this.$axios
+					.$get(`api/product/` + this.$route.params.id)
+					.then(res => {
+						this.$set(this.$data, "form", res.product);
+						console.log(res);
+					})
+					.catch(err => {
+						console.log(err.response);
+					});
+			},
+
+			updateItem() {
+				let rawData = {
 					code: this.form.code,
+					name: this.form.name,
 					barcode: this.form.barcode,
 					unit: this.form.unit,
 					price: this.form.price,
-					brand: this.form.brand,
-					image: this.form.image,
 					description: this.form.description,
-				})
-		    	.then(res => {
-		    		this.items = res.data;
-		    		this.$toast.info('Succeessfully created');
-		    		this.$router.push('/product/product-list');
-		    	})
-		    	.catch(err => {
-					console.log(err.response);
-					this.$refs.form.validate(
-						err.response.data.errors
-					);
-		    	})
-		    },
+					_method: "PATCH"
+				};
+				let fd = new FormData();
 
-		    uploadImage(e) {
-                const images = e.target.files[0];
-                const reader = new FileReader();
+				Object.keys(rawData).map(e => {
+					fd.append(e, rawData[e]);
+				});
 
-                reader.readAsDataURL(images);
-                reader.onload = e =>  {
-                    this.form.image = e.target.result;
-                    console.log(this.form);
-                }
+				fd.append("image", this.file);
 
-                this.url = URL.createObjectURL(images)
-            },
-    	}
-	}
+				// append Request Object
+				fd.append("category", JSON.stringify(this.form.category));
+
+				this.$axios
+					.$post(`api/product/${this.form.id}`, fd, {
+						headers: {
+							"Content-Type": "multipart/form-data"
+						}
+					})
+					.then(res => {
+						console.log(fd);
+						this.items = res.data;
+						this.$toast.info("Succeessfully created");
+						this.$router.push("/product/product-list");
+					})
+					.catch(err => {
+						console.log(err.response);
+						this.$refs.form.validate(err.response.data.errors);
+					});
+			},
+
+			uploadImage(e) {
+				this.file = e.target.files[0];
+			}
+		}
+	};
 </script>
 
 <style lang="scss">
